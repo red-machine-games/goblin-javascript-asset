82b2d53bce18247adb53e5c10a466566c5df69e3
Andrew Medvedev <andrew.medvedev.nh@gmail.com>
2019-08-21 04:33:51 +0500

    readme fix

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Node.js(HTML5) Gbase asset
 
+[![Latest Documentation](https://doxdox.org/images/badge-flat.svg)](https://doxdox.org/red-machine-games/goblin-javascript-asset)
+
 This is javascript asset to work with Goblin Backend: https://gbase.tech/
 
  - All dependencies are browser-compatible
--- index.js
+++ index.js
@@ -4,10 +4,15 @@
  * To work with API you need to instantiate GbaseApi object.
  */
 
-global.Gbase = {
+var Gbase = {
     GbaseApi: require('./lib/GbaseApi.js'),
     GbaseResponse: require('./lib/objects/GbaseResponse.js'),
     GbaseError: require('./lib/objects/GbaseError.js'),
     GbaseRangePicker: require('./lib/objects/GbaseRangePicker.js'),
     GbasePvp: require('./lib/pvp/GbasePvp.js')
 };
+
+module.exports = {
+    Gbase
+};
+global.Gbase = Gbase;
